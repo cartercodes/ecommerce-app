72c81403491ef1d8e5ccbd9538aa87964ae318ae
Mitchell Carter <carter.svo@gmail.com>
2019-01-31 14:46:25 -0500

    built base for shop component. built shop route etc

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -15,6 +15,7 @@ import Layout from './components/layout';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import Account from './components/account/account';
+import Shop from './components/shop/shop';
 
 function main() {
   ReactDOM.render(
@@ -24,9 +25,12 @@ function main() {
           <Switch>
             <Route path='/' exact component={Signin}/>
             <Route path='/signin' exact component={Signin}/>
+
             <Route path='/signup' exact component={Signup}/>
 
             <Route path='/account' exact component={Account}/>
+
+            <Route path='/shop' exact component={Shop}/>
           </Switch>
         </Layout>
       </Router>
