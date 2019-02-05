c02df272f17305dc6a60161f65756faba3c2d00e
Mitchell Carter <carter.svo@gmail.com>
2019-02-05 14:02:20 -0500

    built review product component

--- src/components/greenPriceTag.js
+++ src/components/greenPriceTag.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class GreenPriceTag extends Component {
     render() {
-        const {className, title } = this.props;
+        const { className, title } = this.props;
         return (
             <div className={`${className} green-price-tag`}>
                 ${title}
